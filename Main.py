51ccfcb9e05c01ed0490a548292d236a729a49ce
samin akbari <saminakbari.e@gmail.com>
2024-07-09 17:26:16 +0330

    read from file debugged

--- Main.py
+++ Main.py
@@ -26,16 +26,14 @@ def show_history(username):
     content = ""
     lines = read_from_file("history.txt")
     for game_number in range(len(lines) // 4):
-        if re.search(pattern, lines[game_number]) or re.search(pattern, lines[game_number + 1]):
-            content += lines[game_number] + lines[game_number + 1] + lines[game_number + 2] + lines[game_number + 3]
+        line_number = game_number * 4
+        if re.search(pattern, lines[line_number]) or re.search(pattern, lines[line_number + 1]):
+            content += lines[line_number] + lines[line_number + 1] + lines[line_number + 2] + lines[line_number + 3]
 
     print("Your history:\n" + content)
 
 
-with open("history.txt", "w") as f:
-    f.write("")
 
-print("Welcome to Tic Tac Toe game!")
 
 
 def get_name():
@@ -83,4 +81,7 @@ def run_program():
     run_program()
 
 
+with open("history.txt", "w") as f:
+    f.write("")
+print("Welcome to Tic Tac Toe game!")
 run_program()
